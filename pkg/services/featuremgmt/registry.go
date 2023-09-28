7044bbc32dd0fcf93401d0b6a32abbf79e481ffb
suntala <arati.rana@grafana.com>
2023-09-28 14:28:43 +0200

    Fix feature toggle files

--- pkg/services/featuremgmt/registry.go
+++ pkg/services/featuremgmt/registry.go
@@ -817,12 +817,6 @@ var (
 			Owner:        grafanaAlertingSquad,
 		},
 		{
-			Name:        "panelTitleSearchInV1",
-			Description: "Enable searching for dashboards using panel title in search v1",
-			Stage:       FeatureStageExperimental,
-			Owner:       grafanaBackendPlatformSquad,
-		},
-		{
 			Name:            "panelTitleSearchInV1",
 			Description:     "Enable searching for dashboards using panel title in search v1",
 			RequiresDevMode: true,
